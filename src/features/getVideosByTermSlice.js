b705d13677afb06671abb42a90672e5377e713a0
vtejaeta <vtejaeta@gmail.com>
2021-04-21 19:14:48 +0530

    Add code to get data from youtube api

--- src/features/getVideosByTermSlice.js
+++ src/features/getVideosByTermSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import youtube from "d:/videos_hooks/src/apis/youtube";
 
 const initialState = {
   videosByTerm: [],
@@ -9,14 +10,34 @@ const getVideosByTermSlice = createSlice({
   initialState,
   reducers: {
     setVideosByTerm: (state, action) => {
-      state.videosByTerm = [...action.payload];
+      state.videosByTerm = action.payload;
     },
   },
 });
 
-export const searchVideosByTerm = () => async (dispatch) => {
-  dispatch(setVideosByTerm());
+export const searchVideosByTerm = (searchTerm) => async (dispatch) => {
+  const { data } = await youtube.get("/search", {
+    params: {
+      q: searchTerm,
+      type: "video",
+      part: "snippet",
+      maxResults: 10,
+      key: process.env.REACT_APP_YOUTUBE_APIKEY,
+    },
+  });
+  dispatch(setVideosByTerm(data.items));
 };
+
+export async function getVideoViewsCount(videoId) {
+  const { data } = await youtube.get("/videos", {
+    params: {
+      part: "statistics",
+      id: videoId,
+      key: process.env.REACT_APP_YOUTUBE_APIKEY,
+    },
+  });
+  return data;
+}
 
 export const { setVideosByTerm } = getVideosByTermSlice.actions;
 export default getVideosByTermSlice.reducer;
