7507b7a8e8e1c3d89aac2ff2d744448c94d0c2e6
vtejaeta <vtejaeta@gmail.com>
2021-04-21 19:16:17 +0530

    Add use of videoGrid component

--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -1,37 +1,27 @@
 import { navigate } from "@reach/router";
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import VideoCard from "../../components/layout/videoCard/VideoCard";
+import VideosGridLayout from "../../components/layout/videosGridLayout/VideosGridLayout";
+import { searchVideosByTerm } from "../../features/getVideosByTermSlice";
 import "./homeScreen.styles.scss";
 
 export default function HomeScreen() {
   const { userName, userEmail, userPhoto } = useSelector((state) => state.user);
+  const { videosByTerm } = useSelector((state) => state.getVideosByTerm);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     !userName && navigate("/");
-  }, [userName]);
+    if (!videosByTerm.length) {
+      dispatch(searchVideosByTerm("ReactJS"));
+    }
+  }, [userName, videosByTerm, dispatch]);
 
   return (
     <div className="home-screen-cont">
       <div className="whole-videos-cont">
-        <div className="video-section-cont">
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-        </div>
-        <div className="video-section-cont">
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-          <VideoCard />
-        </div>
+        <VideosGridLayout />
       </div>
     </div>
   );
