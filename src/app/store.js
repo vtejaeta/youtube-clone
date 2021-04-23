56e64c040e05a84d72838ce454818f35b7366e89
vtejaeta <vtejaeta@gmail.com>
2021-04-23 21:09:42 +0530

    Add slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import getVideosByTermReducer from "../features/getVideosByTermSlice";
+import themeReducer from "../features/themeSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     getVideosByTerm: getVideosByTermReducer,
+    theme: themeReducer,
   },
 });
