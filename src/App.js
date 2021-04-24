9be3d3b673343fdfc52ead524e3b4228efb3a08e
vtejaeta <vtejaeta@gmail.com>
2021-04-24 17:53:07 +0530

    Add navigation to search results page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Router } from "@reach/router";
 import SignInScreen from "./screens/signInScreen/SignInScreen";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
+import SearchResultsScreen from "./screens/searchResultsScreen/SearchResultsScreen";
 
 function App() {
   return (
     <Router>
       <SignInScreen path="/" />
       <HomeScreen path="/home" />
+      <SearchResultsScreen path="/results" />
     </Router>
   );
 }
