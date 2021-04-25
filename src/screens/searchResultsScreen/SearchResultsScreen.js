01f8abd83f5c55d8992716ff645c2e8c05b01fac
vtejaeta <vtejaeta@gmail.com>
2021-04-25 19:40:01 +0530

    Add cleanup function in useEffect

--- src/screens/searchResultsScreen/SearchResultsScreen.js
+++ src/screens/searchResultsScreen/SearchResultsScreen.js
@@ -1,21 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "../../components/layout/header/Header";
 import VideosGridLayout from "../../components/layout/videosGridLayout/VideosGridLayout";
-import { searchVideosByTerm } from "../../features/getVideosByTermSlice";
+import {
+  resetVideos,
+  searchVideosByTerm,
+} from "../../features/getVideosByTermSlice";
+import "../homeScreen/homeScreen.styles.scss";
 
 export default function SearchResultsScreen(props) {
   const [searchTerm, setSearchTerm] = useState(null);
+  const { userName, userEmail, userPhoto } = useSelector((state) => state.user);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     const term = new URL(props.location.href).searchParams.get("search_term");
     setSearchTerm(term.trim());
     searchTerm && dispatch(searchVideosByTerm(searchTerm));
+
+    return () => dispatch(resetVideos());
   }, [props, dispatch, searchTerm]);
 
   return (
     <div className="whole-videos-cont">
-      <VideosGridLayout />
+      <Header userPhoto={userPhoto} userEmail={userEmail} userName={userName} />
+      <div className="video-section-cont">
+        <VideosGridLayout />
+      </div>
     </div>
   );
 }
