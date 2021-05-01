8474c42aabae0cba744a62025e2acda1910589ec
vtejaeta <vtejaeta@gmail.com>
2021-05-01 21:17:26 +0530

    refactor to remove state of search term

--- src/screens/searchResultsScreen/SearchResultsScreen.js
+++ src/screens/searchResultsScreen/SearchResultsScreen.js
@@ -1,5 +1,4 @@
-import { useLocation } from "@reach/router";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/layout/header/Header";
 import VideosGridLayout from "../../components/layout/videosGridLayout/VideosGridLayout";
@@ -7,27 +6,20 @@ import {
   resetVideos,
   searchVideosByTerm,
 } from "../../features/getVideosByTermSlice";
-
 import "../homeScreen/homeScreen.styles.scss";
 
-export default function SearchResultsScreen() {
-  const [searchTerm, setSearchTerm] = useState(null);
-  const location = useLocation();
+export default function SearchResultsScreen(props) {
   const { userName, userEmail, userPhoto } = useSelector((state) => state.user);
   const { theme: UITheme } = useSelector((state) => state.theme);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const term = new URL(location.href).searchParams.get("search_term");
-    term.trim() !== searchTerm && setSearchTerm(term.trim());
-    if (searchTerm) {
-      dispatch(resetVideos());
-      dispatch(searchVideosByTerm(searchTerm));
-    }
+    const term = new URL(props.location.href).searchParams.get("search_term");
+    dispatch(searchVideosByTerm(term));
 
-    // return () => dispatch(resetVideos());
-  }, [location, dispatch, searchTerm]);
+    return () => dispatch(resetVideos());
+  }, [props.location, dispatch]);
 
   return (
     <div
