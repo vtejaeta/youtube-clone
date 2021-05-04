ab91856612bf09909c2de2073678381929637a34
vtejaeta <vtejaeta@gmail.com>
2021-05-04 18:11:58 +0530

    Add custom hook for dispatch api call

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,21 +1,15 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   resetVideos,
   searchVideosByTerm,
 } from "../features/getVideosByTermSlice";
 
-export default function useVideos(searchTerm) {
+export default function useVideos(searchTerm = "ReactJS") {
   const dispatch = useDispatch();
-
-  const { videosByTerm } = useSelector((state) => state.getVideosByTerm);
 
   useEffect(() => {
     dispatch(resetVideos());
     dispatch(searchVideosByTerm(searchTerm));
-
-    // return () => dispatch(resetVideos());
   }, [dispatch, searchTerm]);
-
-  return videosByTerm;
 }
