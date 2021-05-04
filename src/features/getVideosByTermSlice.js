726fa26a9bec54b9b4cd88c5439d539260b3eaf4
vtejaeta <vtejaeta@gmail.com>
2021-05-04 18:11:05 +0530

    Add loading, error to store for api call status

--- src/features/getVideosByTermSlice.js
+++ src/features/getVideosByTermSlice.js
@@ -2,15 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 import youtube from "d:/videos_hooks/src/apis/youtube";
 
 const initialState = {
+  loading: false,
   videosByTerm: [],
+  error: null,
 };
 
 const getVideosByTermSlice = createSlice({
   name: "getVideosByTerm",
   initialState,
   reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
     setVideosByTerm: (state, action) => {
       state.videosByTerm = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
     },
     resetVideos: (state) => {
       state.videosByTerm = [];
@@ -19,28 +27,41 @@ const getVideosByTermSlice = createSlice({
 });
 
 export const searchVideosByTerm = (searchTerm) => async (dispatch) => {
-  const { data } = await youtube.get("/search", {
-    params: {
-      q: searchTerm,
-      type: "video",
-      part: "snippet",
-      maxResults: 10,
-      key: process.env.REACT_APP_YOUTUBE_APIKEY,
-    },
-  });
-  dispatch(setVideosByTerm(data.items));
+  dispatch(setLoading(true));
+  try {
+    const { data } = await youtube.get("/search", {
+      params: {
+        q: searchTerm,
+        type: "video",
+        part: "snippet",
+        maxResults: 10,
+        key: process.env.REACT_APP_YOUTUBE_APIKEY,
+      },
+    });
+    dispatch(setVideosByTerm(data.items));
+  } catch (error) {
+    dispatch(setError(error));
+  } finally {
+    dispatch(setLoading(false));
+  }
 };
 
-export async function getVideoViewsCount(videoId) {
-  const { data } = await youtube.get("/videos", {
-    params: {
-      part: "statistics",
-      id: videoId,
-      key: process.env.REACT_APP_YOUTUBE_APIKEY,
-    },
-  });
-  return data;
-}
+// export async function getVideoViewsCount(videoId) {
+//   const { data } = await youtube.get("/videos", {
+//     params: {
+//       part: "statistics",
+//       id: videoId,
+//       key: process.env.REACT_APP_YOUTUBE_APIKEY,
+//     },
+//   });
+//   return data;
+// }
 
-export const { setVideosByTerm, resetVideos } = getVideosByTermSlice.actions;
+export const {
+  setLoading,
+  setVideosByTerm,
+  setError,
+  resetVideos,
+} = getVideosByTermSlice.actions;
+
 export default getVideosByTermSlice.reducer;
