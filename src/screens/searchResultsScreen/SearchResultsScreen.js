4c2ca04f9d71f832de2f50595e802c5f1e70a493
vtejaeta <vtejaeta@gmail.com>
2021-05-05 21:15:06 +0530

    Refactor code to remove side effects

--- src/screens/searchResultsScreen/SearchResultsScreen.js
+++ src/screens/searchResultsScreen/SearchResultsScreen.js
@@ -1,5 +1,4 @@
-import { navigate } from "@reach/router";
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/layout/header/Header";
 import SkeletonVideoGrid from "../../components/layout/skeletons/SkeletonVideoGrid";
@@ -9,7 +8,6 @@ import useVideosStateFromRedux from "../../hooks/useVideosStateFromRedux";
 import "../homeScreen/homeScreen.styles.scss";
 
 export default function SearchResultsScreen(props) {
-  const { userName, userEmail, userPhoto } = useSelector((state) => state.user);
   const { theme: UITheme } = useSelector((state) => state.theme);
 
   // const dispatch = useDispatch();
@@ -18,29 +16,14 @@ export default function SearchResultsScreen(props) {
 
   useVideos(term);
 
-  // useEffect(() => {
-  //   const term = new URL(props.location.href).searchParams.get("search_term");
-  //   dispatch(searchVideosByTerm(term));
-
-  //   return () => dispatch(resetVideos());
-  // }, [props.location, dispatch]);
-
-  useEffect(() => {
-    !userName && navigate("/");
-  }, [userName]);
-
-  return userName ? (
+  return (
     <div
       className={
         UITheme === "dark" ? "home-screen-cont dark" : "home-screen-cont"
       }
     >
       <div className="whole-videos-cont">
-        <Header
-          userPhoto={userPhoto}
-          userEmail={userEmail}
-          userName={userName}
-        />
+        <Header />
         {loading ? (
           <SkeletonVideoGrid />
         ) : (
@@ -50,7 +33,5 @@ export default function SearchResultsScreen(props) {
         )}
       </div>
     </div>
-  ) : (
-    <p>Loading...</p>
   );
 }
