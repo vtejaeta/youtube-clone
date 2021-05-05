e3d2bb308fee72d9ff1b1b4c3fc2ce900b5f7744
vtejaeta <vtejaeta@gmail.com>
2021-05-05 21:13:03 +0530

    Add protected route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { Router } from "@reach/router";
+import React, { useEffect, useState } from "react";
+import { Redirect, Router } from "@reach/router";
 import SignInScreen from "./screens/signInScreen/SignInScreen";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
 import SearchResultsScreen from "./screens/searchResultsScreen/SearchResultsScreen";
+import { getFromLocalStorage } from "./utils/localStorage.utils";
+
+const ProtectedRoute = ({ component: Component, ...rest }) =>
+  getFromLocalStorage("_userEmail") ? (
+    <Component {...rest} />
+  ) : (
+    <Redirect from="" to="/" noThrow />
+  );
 
 function App() {
   return (
     <Router>
-      <SignInScreen path="/" />
-      <HomeScreen path="/home" />
-      <SearchResultsScreen path="/results" />
+      <SignInScreen path="/" default />
+      <ProtectedRoute component={HomeScreen} path="/home" />
+      <ProtectedRoute component={SearchResultsScreen} path="/results" />
     </Router>
   );
 }
