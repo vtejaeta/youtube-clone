d35e54c6aa5ee609ed8e0e130a5513af52d7eefb
vtejaeta <vtejaeta@gmail.com>
2021-05-06 21:59:44 +0530

    Refactor code to reflect new file name

--- src/screens/searchResultsScreen/SearchResultsScreen.js
+++ src/screens/searchResultsScreen/SearchResultsScreen.js
@@ -2,15 +2,16 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Header from "../../components/layout/header/Header";
 import SkeletonVideoGrid from "../../components/layout/skeletons/SkeletonVideoGrid";
-import VideosGridLayout from "../../components/layout/videosGridLayout/VideosGridLayout";
+import VideosLayout from "../../components/layout/videosLayout/VideosLayout";
+import ResultScreenVideoCard from "../../components/shared/resultScreenVideoCard/ResultScreenVideoCard";
 import useVideos from "../../hooks/useVideos";
 import useVideosStateFromRedux from "../../hooks/useVideosStateFromRedux";
 import "../homeScreen/homeScreen.styles.scss";
+import "../searchResultsScreen/searchResultsScreen.styles.scss";
 
 export default function SearchResultsScreen(props) {
   const { theme: UITheme } = useSelector((state) => state.theme);
 
-  // const dispatch = useDispatch();
   const term = new URL(props.location.href).searchParams.get("search_term");
   const { loading } = useVideosStateFromRedux();
 
@@ -27,8 +28,8 @@ export default function SearchResultsScreen(props) {
         {loading ? (
           <SkeletonVideoGrid />
         ) : (
-          <div className="video-section-cont">
-            <VideosGridLayout />
+          <div className="video-section-cont search-results">
+            <VideosLayout Component={ResultScreenVideoCard} />
           </div>
         )}
       </div>
