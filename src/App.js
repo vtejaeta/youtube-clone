ba6a3bed7e390eee25b9f9327b00cb13b963f3a6
vtejaeta <vtejaeta@gmail.com>
2021-05-08 19:40:35 +0530

    Add working programmatic navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Redirect, Router } from "@reach/router";
 import SignInScreen from "./screens/signInScreen/SignInScreen";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
 import SearchResultsScreen from "./screens/searchResultsScreen/SearchResultsScreen";
-import { getFromLocalStorage } from "./utils/localStorage.utils";
+import useUserStateFromRedux from "./hooks/useUserStateFromRedux";
 
-const ProtectedRoute = ({ component: Component, ...rest }) =>
-  getFromLocalStorage("_userEmail") ? (
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { userEmail } = useUserStateFromRedux();
+  return userEmail ? (
     <Component {...rest} />
   ) : (
     <Redirect from="" to="/" noThrow />
   );
+};
+
+const DefaultRoute = ({ component: Component, ...rest }) => {
+  const { userEmail } = useUserStateFromRedux();
+  return userEmail ? (
+    <Redirect from="" to="/home" noThrow />
+  ) : (
+    <Component {...rest} />
+  );
+};
 
 function App() {
   return (
     <Router>
-      <SignInScreen path="/" />
+      <DefaultRoute component={SignInScreen} path="/" />
       <ProtectedRoute component={HomeScreen} path="/home" />
       <ProtectedRoute component={SearchResultsScreen} path="/results" />
     </Router>
