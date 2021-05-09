b634c20ca4815d6838bf55a26ccc53f6f09d6826
vtejaeta <vtejaeta@gmail.com>
2021-05-09 21:16:32 +0530

    Add illustrator image for empty search result

--- src/components/layout/videosLayout/VideosLayout.js
+++ src/components/layout/videosLayout/VideosLayout.js
@@ -1,12 +1,25 @@
 import React from "react";
 import moment from "moment";
 import useVideosStateFromRedux from "../../../hooks/useVideosStateFromRedux";
+import Empty_Pana from "../../../assets/Empty-pana.svg";
 
 export default function VideosLayout({ Component }) {
   const { videosByTerm } = useVideosStateFromRedux();
 
   function renderVideos() {
-    return videosByTerm?.map(({ snippet, id }) => {
+    if (videosByTerm.length === 0) {
+      return (
+        <>
+          <img
+            src={Empty_Pana}
+            alt="Empty results"
+            className="empty-results-logo"
+          />
+          <strong className="empty-results">No results found</strong>
+        </>
+      );
+    }
+    return videosByTerm.map(({ snippet, id }) => {
       const timeFromNow = moment(
         new Date(snippet.publishTime).toLocaleString()
       ).fromNow();
