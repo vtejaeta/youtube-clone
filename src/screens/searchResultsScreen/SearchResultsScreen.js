f370965d83f959c4997e6d25f97380cdd4fa7f52
vtejaeta <vtejaeta@gmail.com>
2021-05-09 21:18:39 +0530

    Add utility function to get searchParam

--- src/screens/searchResultsScreen/SearchResultsScreen.js
+++ src/screens/searchResultsScreen/SearchResultsScreen.js
@@ -8,11 +8,13 @@ import useVideosStateFromRedux from "../../hooks/useVideosStateFromRedux";
 import useThemeStateFromRedux from "../../hooks/useThemeStateFromRedux";
 import "../homeScreen/homeScreen.styles.scss";
 import "../searchResultsScreen/searchResultsScreen.styles.scss";
+import SkeletonSearchVideoResults from "../../components/layout/skeletons/SkeletonSearchVideoResults";
+import getSearchParam from "../../utils/searchParam.utils";
 
 export default function SearchResultsScreen(props) {
   const { theme: UITheme } = useThemeStateFromRedux();
 
-  const term = new URL(props.location.href).searchParams.get("search_term");
+  const term = getSearchParam(props.location, "search_term");
   const { loading } = useVideosStateFromRedux();
 
   useVideos(term);
@@ -26,10 +28,18 @@ export default function SearchResultsScreen(props) {
       <div className="whole-videos-cont">
         <Header />
         {loading ? (
-          <SkeletonVideoGrid />
+          <SkeletonSearchVideoResults />
         ) : (
-          <div className="video-section-cont search-results">
-            <VideosLayout Component={ResultScreenVideoCard} />
+          <div className="main-videos-cont">
+            {term && (
+              <p className="search-term-desc">
+                Search results for{" "}
+                <strong style={{ textTransform: "capitalize" }}>{term}</strong>
+              </p>
+            )}
+            <div className="video-section-cont search-results">
+              <VideosLayout Component={ResultScreenVideoCard} />
+            </div>
           </div>
         )}
       </div>
