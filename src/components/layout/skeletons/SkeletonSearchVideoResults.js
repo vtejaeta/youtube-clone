1e0b5e86e273c80a9ae7c6c37e7d1b7302e63f10
vtejaeta <vtejaeta@gmail.com>
2021-05-09 21:12:39 +0530

    Add skeleton for search videos results

---
+++ src/components/layout/skeletons/SkeletonSearchVideoResults.js
@@ -0,0 +1,50 @@
+import React from "react";
+import useThemeStateFromRedux from "../../../hooks/useThemeStateFromRedux";
+import { v4 as uuidv4 } from "uuid";
+import SkeletonElement from "./SkeletonElement";
+import "../../../screens/searchResultsScreen/searchResultsScreen.styles.scss";
+
+export default function SkeletonSearchVideoResults() {
+  const { theme: UITheme } = useThemeStateFromRedux();
+
+  function renderSkeletonCard() {
+    return (
+      <div className="skeleton-search-results">
+        <SkeletonElement type="thumbnail" />
+        <div className="skeleton-video-author-details">
+          <SkeletonElement type="title" />
+          <SkeletonElement type="meta-text" />
+          <div className="skeleton-channel-details">
+            <SkeletonElement type="avatar" />
+            <SkeletonElement type="text" />
+          </div>
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
+          <SkeletonElement type="text" />
+        </div>
+      </div>
+    );
+  }
+
+  function renderSkeletonGrid() {
+    return Array(10)
+      .fill(1)
+      .map(() => {
+        return (
+          <React.Fragment key={uuidv4()}>{renderSkeletonCard()}</React.Fragment>
+        );
+      });
+  }
+
+  return (
+    <div
+      className={
+        UITheme === "dark"
+          ? "search-results-video-container-skeleton dark"
+          : "search-results-video-container-skeleton"
+      }
+    >
+      {renderSkeletonGrid()}
+    </div>
+  );
+}
