108a827215d7bdc4236d0099ce5ea8bb77e07f3f
vtejaeta <vtejaeta@gmail.com>
2021-05-11 20:10:27 +0530

    Initial boilerplate for watchScreen

--- src/screens/watchVideoScreen/WatchVideoScreen.js
+++ src/screens/watchVideoScreen/WatchVideoScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import getSearchParam from "../../utils/searchParam.utils";
 import Header from "../../components/layout/header/Header";
 import "./watchVideoScreen.styles.scss";
+import HomeScreenVideoCard from "../../components/shared/homeScreenVideoCard/HomeScreenVideoCard";
+import VideosLayout from "../../components/layout/videosLayout/VideosLayout";
 
 export default function WatchVideoScreen(props) {
   const term = getSearchParam(props.location, "v");
@@ -10,14 +12,19 @@ export default function WatchVideoScreen(props) {
     <>
       <Header />
       <div className="whole-video-wrapper">
-        <div className="main-video-player-cont">
-          <iframe
+        <div className="grid-cont">
+          <div className="main-video-player-cont">
+            {/* <iframe
             src={`https://www.youtube.com/embed/${term}`}
             title="video player"
             frameBorder="0"
             allowFullScreen
             className="main-video-player"
-          />
+          /> */}
+          </div>
+          <div className="next-videos-cont">
+            <VideosLayout Component={HomeScreenVideoCard} />
+          </div>
         </div>
       </div>
       {/* <div className="ui segment">
