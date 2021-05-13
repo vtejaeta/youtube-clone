c04cb9de78afb06286fc076210988591d6d57c5d
vtejaeta <vtejaeta@gmail.com>
2021-05-13 20:50:14 +0530

    Add suggestion videos into main player screen

--- src/screens/watchVideoScreen/WatchVideoScreen.js
+++ src/screens/watchVideoScreen/WatchVideoScreen.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import getSearchParam from "../../utils/searchParam.utils";
 import Header from "../../components/layout/header/Header";
 import "./watchVideoScreen.styles.scss";
-import ResultScreenVideoCard from "../../components/shared/resultScreenVideoCard/ResultScreenVideoCard";
-import VideosLayout from "../../components/layout/videosLayout/VideosLayout";
+import { useDispatch } from "react-redux";
+import {
+  resetVideos,
+  searchSuggestedVideos,
+} from "../../features/getSuggestionVideosSlice";
+import SuggestionVideosLayout from "../../components/layout/suggestionVideosLayout/SuggestionVideosLayout";
+import useSuggestionVideosFromRedux from "../../hooks/useSuggestionVideosFromRedux";
+import SkeletonSearchVideoResults from "../../components/layout/skeletons/SkeletonSearchVideoResults";
+import useThemeStateFromRedux from "../../hooks/useThemeStateFromRedux";
 
 export default function WatchVideoScreen(props) {
+  const dispatch = useDispatch();
   const term = getSearchParam(props.location, "v");
+  const { loading } = useSuggestionVideosFromRedux();
+  const { theme } = useThemeStateFromRedux();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    dispatch(resetVideos());
+    dispatch(searchSuggestedVideos(term));
+
+    return () => dispatch(resetVideos());
+  }, [dispatch, term]);
 
   return (
     <>
       <Header />
-      <div className="whole-video-wrapper">
+      <div
+        className={
+          theme === "dark" ? "whole-video-wrapper dark" : "whole-video-wrapper"
+        }
+      >
         <div className="grid-cont">
           <div className="main-video-player-cont">
             <iframe
@@ -24,7 +46,11 @@ export default function WatchVideoScreen(props) {
           </div>
           <div className="next-videos-cont">
             <button className="suggestion-text">Suggestion Videos</button>
-            <VideosLayout Component={ResultScreenVideoCard} />
+            {loading ? (
+              <SkeletonSearchVideoResults />
+            ) : (
+              <SuggestionVideosLayout />
+            )}
           </div>
         </div>
       </div>
