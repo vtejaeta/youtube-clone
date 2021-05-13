b21d3b3b3440b01aabc9fc5f4f9f6fe9b838e0ef
vtejaeta <vtejaeta@gmail.com>
2021-05-13 20:47:16 +0530

    Add new reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import getVideosByTermReducer from "../features/getVideosByTermSlice";
 import themeReducer from "../features/themeSlice";
+import getSuggestionVideosReducer from "../features/getSuggestionVideosSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     getVideosByTerm: getVideosByTermReducer,
+    getSuggestionVideos: getSuggestionVideosReducer,
     theme: themeReducer,
   },
 });
