b94430eaaa351a506d41668349d1a65f77eac361
vtejaeta <vtejaeta@gmail.com>
2021-05-13 20:48:55 +0530

    Add new component for suggested videos

---
+++ src/components/layout/suggestionVideosLayout/SuggestionVideosLayout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import moment from "moment";
+import useSuggestionVideosFromRedux from "../../../hooks/useSuggestionVideosFromRedux";
+import Empty_Pana from "../../../assets/Empty-pana.svg";
+import ResultScreenVideoCard from "../../shared/resultScreenVideoCard/ResultScreenVideoCard";
+
+export default function SuggestionVideosLayout() {
+  const { suggestionVideos } = useSuggestionVideosFromRedux();
+
+  function renderVideos() {
+    if (suggestionVideos.length === 0) {
+      return (
+        <>
+          <img
+            src={Empty_Pana}
+            alt="Empty results"
+            className="empty-results-logo"
+          />
+          <strong className="empty-results">No results found</strong>
+        </>
+      );
+    }
+    return suggestionVideos
+      .filter(({ snippet }) => snippet !== undefined)
+      .map(({ snippet, id }) => {
+        const timeFromNow = moment(
+          new Date(snippet.publishTime).toLocaleString()
+        ).fromNow();
+        return (
+          <ResultScreenVideoCard
+            key={id.videoId}
+            timeFromNow={timeFromNow}
+            thumbnailURL={snippet.thumbnails.medium.url}
+            videoId={id.videoId}
+            title={snippet.title}
+            description={snippet.description}
+            channelTitle={snippet.channelTitle}
+          />
+        );
+      });
+  }
+
+  return <>{renderVideos()}</>;
+}
