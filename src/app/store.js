6f40fd92b98e63c41d17de7dd5161b4c17731aea
vtejaeta <vtejaeta@gmail.com>
2021-05-19 21:51:37 +0530

    Add new slice reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import userReducer from "../features/userSlice";
 import getVideosByTermReducer from "../features/getVideosByTermSlice";
 import themeReducer from "../features/themeSlice";
 import getSuggestionVideosReducer from "../features/getSuggestionVideosSlice";
+import getVideoDetailsReducer from "../features/getVideoDetailsSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     getVideosByTerm: getVideosByTermReducer,
     getSuggestionVideos: getSuggestionVideosReducer,
+    getVideoDetails: getVideoDetailsReducer,
     theme: themeReducer,
   },
 });
