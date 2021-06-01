7ad74cd362c90762eb56680d9d92ddc99cb5a0e7
vtejaeta <vtejaeta@gmail.com>
2021-06-01 17:01:17 +0530

    Refactor code

--- src/screens/signInScreen/SignInScreen.js
+++ src/screens/signInScreen/SignInScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import YoutubeLight from "../../assets/youtube-light.png";
+import YoutubeDark from "../../assets/youtube-dark.png";
 import SignInButton from "../../components/shared/signInButton/SignInButton";
 import "./signInScreen.styles.scss";
 
@@ -7,7 +7,7 @@ export default function SignInScreen() {
   return (
     <div className="sign-in-card">
       <div className="hero-logo-cont">
-        <img src={YoutubeLight} alt="Youtube main logo" className="hero-logo" />
+        <img src={YoutubeDark} alt="Youtube main logo" className="hero-logo" />
       </div>
       <p>Please use below Sign-In to proceed further.</p>
       <hr />
