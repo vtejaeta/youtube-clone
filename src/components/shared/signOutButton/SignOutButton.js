3dc201491d38df4cde5ba33ba9d4c56523d9e3f7
vtejaeta <vtejaeta@gmail.com>
2021-06-15 18:05:07 +0530

    Abstract sign out button into component

---
+++ src/components/shared/signOutButton/SignOutButton.js
@@ -0,0 +1,25 @@
+import { navigate } from "@reach/router";
+import React from "react";
+import { logUserOut } from "../../../features/userSlice";
+import useUserStateFromRedux from "../../../hooks/useUserStateFromRedux";
+
+export default function SignOutButton({ dispatch }) {
+  const { userPhoto } = useUserStateFromRedux();
+
+  return (
+    <div className="menu-items dropdown">
+      <img src={userPhoto} alt="User info" />
+      <div className="dropdown-content">
+        <button
+          className="sign-out-btn"
+          onClick={() => {
+            dispatch(logUserOut());
+            navigate("/");
+          }}
+        >
+          Sign out
+        </button>
+      </div>
+    </div>
+  );
+}
