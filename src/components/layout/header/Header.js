7a793dbf378c575b8e61375b8d48c76c541b5f34
vtejaeta <vtejaeta@gmail.com>
2021-06-15 18:06:43 +0530

    Refactor

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,74 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { Link, navigate } from "@reach/router";
-import youtubeLight from "../../../assets/youtube-light.png";
+import React from "react";
+import { navigate } from "@reach/router";
 import searchIcon from "../../../assets/search-icon.svg";
 import backArrowBlack from "../../../assets/arrow_back_black.svg";
 import { useDispatch } from "react-redux";
 import "./header.styles.scss";
 import { setToggleTheme } from "../../../features/themeSlice";
-import { getFromLocalStorage } from "../../../utils/localStorage.utils";
 import { resetVideos } from "../../../features/getVideosByTermSlice";
-import { logUserOut } from "../../../features/userSlice";
 import useUserStateFromRedux from "../../../hooks/useUserStateFromRedux";
 import useThemeStateFromRedux from "../../../hooks/useThemeStateFromRedux";
+import ThemeToggleSwitch from "../../shared/themeToggleSwitch/ThemeToggleSwitch";
+import SignOutButton from "../../shared/signOutButton/SignOutButton";
+import YoutubeLogo from "../../shared/youtubeLogo/YoutubeLogo";
 
 export default function Header() {
-  const [theme, setTheme] = useState(
-    () => getFromLocalStorage("_theme") || "light"
-  );
   const headerRef = React.useRef(null);
-  const [searchTerm, setSearchTerm] = useState("");
-  const { userPhoto, userName } = useUserStateFromRedux();
+  const inputRef = React.useRef(null);
+  const { userName } = useUserStateFromRedux();
 
   const { theme: UITheme } = useThemeStateFromRedux();
 
   const dispatch = useDispatch();
 
   function toggleTheme() {
-    setTheme((previousState) => {
-      if (previousState === "light") {
-        return "dark";
-      }
-      return "light";
-    });
+    dispatch(setToggleTheme());
   }
 
-  function inputHandler(e) {
-    setSearchTerm(e.target.value);
-  }
-
-  function activeSearchHandler() {
-    headerRef.current.className =
-      UITheme === "dark" ? "dark active-search" : "active-search";
-  }
-
-  function resetActiveSearch() {
-    headerRef.current.className = UITheme === "dark" ? "dark" : "";
+  function toggleClassName() {
+    headerRef.current.classList.toggle("active-search");
   }
 
   function formHandler(e) {
     e.preventDefault();
-    if (searchTerm.trim()) {
+    let searchTerm = inputRef.current;
+    if (searchTerm.value.trim()) {
       dispatch(resetVideos());
-      navigate(`/results?search_term=${searchTerm.trim()}`);
-      setSearchTerm("");
+      navigate(`/results?search_term=${searchTerm.value.trim()}`);
+      searchTerm.value = "";
     }
   }
 
-  useEffect(() => {
-    if (theme !== UITheme) {
-      dispatch(setToggleTheme(theme));
-    }
-  }, [theme, dispatch, UITheme]);
-
   return (
     <header className={UITheme === "dark" ? "dark" : ""} ref={headerRef}>
-      <Link to="/home" className="main-logo-link-cont">
-        <div className="main-logo-cont">
-          <img src={youtubeLight} alt="youtube-logo" className="header-logo" />
-        </div>
-      </Link>
-      <button type="button" className="back-arrow" onClick={resetActiveSearch}>
+      <YoutubeLogo />
+      <button type="button" className="back-arrow" onClick={toggleClassName}>
         <img
           src={backArrowBlack}
           alt="back arrow"
@@ -81,8 +55,7 @@ export default function Header() {
           aria-label="search"
           placeholder="Search"
           className="search-input"
-          value={searchTerm}
-          onChange={inputHandler}
+          ref={inputRef}
         />
         <button type="submit" className="search-btn">
           <img
@@ -95,7 +68,7 @@ export default function Header() {
       <button
         type="button"
         className="dummy search-btn"
-        onClick={activeSearchHandler}
+        onClick={toggleClassName}
       >
         <img src={searchIcon} alt="search button" className="search-btn-icon" />
       </button>
@@ -103,29 +76,8 @@ export default function Header() {
         <p className="welcome-text">
           Hello <strong>{userName?.toUpperCase()}</strong>
         </p>
-        <label className="switch">
-          <input
-            type="checkbox"
-            className="theme-input"
-            checked={theme === "light" ? false : true}
-            onChange={() => toggleTheme()}
-          />
-          <span className="slider round"></span>
-        </label>
-        <div className="menu-items dropdown">
-          <img src={userPhoto} alt="User info" />
-          <div className="dropdown-content">
-            <button
-              className="sign-out-btn"
-              onClick={(e) => {
-                dispatch(logUserOut());
-                navigate("/");
-              }}
-            >
-              Sign out
-            </button>
-          </div>
-        </div>
+        <ThemeToggleSwitch theme={UITheme} toggleTheme={toggleTheme} />
+        <SignOutButton dispatch={dispatch} />
       </div>
     </header>
   );
